4231d417e5d37d79a289b60e42a56a799f70d0a1
LEYCCI <solsonightkeeper@gmail.com>
2024-06-16 10:41:15 +0330

    New React App

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+<code>src/App.tsx</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://leycci.pro"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          V i s i t
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
